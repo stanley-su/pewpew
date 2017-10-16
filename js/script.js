272106fdbf41ac441d0e94dcca6c344e0a1284dc
Stanley Su <stanleysu00@gmail.com>
2017-10-16 15:40:33 +1100

    added comment outlining code structure and main game object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+/* CODE STRUCTURE:
+ *
+ * GAME: GLOBAL VARIABLES
+ * GAME: SETTINGS
+ * GAME: UTILITY FUNCTIONS
+ * GAME: GAME FUNCTIONS
+ * GAME: FUNCTION EXECUTION
+ *
+ */
+
+const GAME = function() {
+	
+}();
