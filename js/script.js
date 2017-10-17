b77be8f983f9e6e6407fed48e8a2ca91dfa4157d
Stanley Su <stanleysu00@gmail.com>
2017-10-17 17:05:41 +1100

    moved main variables to bottom of GAME function for clearer code

--- js/script.js
+++ js/script.js
@@ -2,14 +2,21 @@
 
 /* CODE STRUCTURE:
  *
- * GAME: GLOBAL VARIABLES
- * GAME: SETTINGS
  * GAME: UTILITY FUNCTIONS
  * GAME: GAME FUNCTIONS
+ * GAME: VARIABLES
  * GAME: FUNCTION EXECUTION
  *
  */
 
 const GAME = function() {
-	
+	// VARIABLES
+
+	const canvas = document.getElementById('canvas');
+	const c = canvas.getContext('2d');
+	const mouse = { x: undefined, y: undefined };
+	const base = { x: innerWidth / 2, y: innerHeight };
+	const cannon = new Cannon(30, '#fff');
+	const projectiles = [];
+	const targets = [];
 }();
