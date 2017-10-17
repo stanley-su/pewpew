1b1b7bdeed22d91f9b9ec72af6362296e1ad888c
Stanley Su <stanleysu00@gmail.com>
2017-10-18 08:13:22 +1100

    added utility functions

--- js/script.js
+++ js/script.js
@@ -10,6 +10,21 @@
  */
 
 const GAME = function() {
+	// UTILITY FUNCTIONS
+
+	// gets a random integer ranging from min to max
+	const getRandomInt = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	};
+
+	// gets 1 or -1
+	const getRandomSign = () => Math.random() < 0.5 ? 1 : -1;
+
+	// returns if 2 objects are colliding
+	const isColliding = function(x1, y1, r1, x2, y2, r2) {
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) <= (r1 + r2);
+	};
+
 	// VARIABLES
 
 	const canvas = document.getElementById('canvas');
